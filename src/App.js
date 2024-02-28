98b249be16312fb7f4b3dec808fc6a465e6efce6
Marc Espinosa i Tejada(W135175) <marc.espinosa@worldline.com>
2024-02-28 11:58:49 +0100

    Updated App page and gitignore for intellij projects

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import logo from './logo.svg';
+import logo2 from './ReactFlow-icon.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a demo project to work with <code>React Flow</code> just try to enter the link.
         </p>
+
+        <img src={logo2} className="App-logo2" alt="logo"/>
+
+        <p/>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
-          Learn React
+          Access React Flow DEMO!!
         </a>
       </header>
     </div>
